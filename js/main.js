9b038021624e123651a8ce190fec7f1a1daeb156
schoolissuperfun <124235072+schoolissuperfun@users.noreply.github.com>
2026-02-09 23:23:56 +0000

    ad

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 //=============================================================================
 // main.js
 //=============================================================================
-
+window.onerror = function(message, source, lineno, colno, error) {
+    console.log("!!! GAME ERROR !!!: " + message + " at " + source + ":" + lineno);
+};
 PluginManager.setup($plugins);
 
 window.onload = function() {
